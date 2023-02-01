c01cbbde91c15a274811c1ec26be5e24b43ff2f0
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-02-01 15:12:32 -0300

    ✨ feat: Create PreRegister form with react hook form

--- src/pages/Home/components/ClaimUsernameForm/index.tsx
+++ src/pages/Home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,31 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Form } from "./styles";
 
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit, reset } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+    reset()
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size={'sm'}
         prefix={'ignite.com/'}
         placeholder={'seu-usuário'}
+        {...register('username')}
       />
 
       <Button size={'sm'} type={'submit'}>
