4d5d715937b79ed12504c9c66f9c2c46dd5d3747
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-02-06 18:21:25 -0300

    📦 refactor: Change GetWeekdays util for get shortWeek names

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,7 +1,15 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((day) => day.substring(0, 1).toUpperCase().concat(day.substring(1)))
+    .map((day) =>
+      short
+        ? day.substring(0, 3).toUpperCase()
+        : day.substring(0, 1).toUpperCase().concat(day.substring(1)),
+    )
 }
