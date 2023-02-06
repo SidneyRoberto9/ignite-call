5533f95887bbc141b786916cf5c586b5ea0a6f12
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-02-06 18:34:11 -0300

    ✨ feat: Add dayjs in calender component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,4 +1,6 @@
+import dayjs from 'dayjs'
 import { CaretLeft, CaretRight } from 'phosphor-react'
+import { useState } from 'react'
 
 import { getWeekDays } from '../../utils/get-week-days'
 import {
@@ -11,19 +13,34 @@ import {
 } from './style'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  function handlePreviousMonth() {
+    setCurrentDate((date) => date.subtract(1, 'month'))
+  }
+
+  function handleNextMonth() {
+    setCurrentDate((date) => date.add(1, 'month'))
+  }
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title={'previous month'}>
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title={'next month'}>
             <CaretRight />
           </button>
         </CalendarActions>
