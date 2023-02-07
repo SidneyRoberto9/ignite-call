129ffe5b3a6d81c4bb57cd2b3ca880d013c99ba3
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-02-07 10:55:01 -0300

    🐛 fix: Redirection after complete

--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -1,17 +1,24 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Avatar, Button, Heading, MultiStep, Text, TextArea } from "@ignite-ui/react";
-import { GetServerSideProps } from "next";
-import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { ArrowRight } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react'
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-import { api } from "../../../lib/axios";
-import { buildNextAuthOptions } from "../../api/auth/[...nextauth].api";
-import { Container, Header } from "../styles";
-import { FormAnnotation, ProfileBox } from "./styles";
+import { api } from '../../../lib/axios'
+import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api'
+import { Container, Header } from '../styles'
+import { FormAnnotation, ProfileBox } from './styles'
 
 const updateProfileSchema = z.object({
   bio: z.string(),
@@ -36,7 +43,7 @@ export default function UpdateProfile() {
       bio: data.bio,
     })
 
-    await router.push(`/schedule/${session.data?.user.id}`)
+    await router.push(`/schedule/${session.data?.user.username}`)
   }
 
   return (
